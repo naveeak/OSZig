5e36ae2b177c494956c5b69c2727bdca3d15f686
ashokmuthusamy99 <ashokmuthusamy99@gmail.com>
2025-01-19 11:35:00 +0530

    Add initial project structure with build configuration and source files

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 OS in thousand lines
 
 Ref : https://operating-system-in-1000-lines.vercel.app/en/
-https://www.youtube.com/watch?v=eAM9ol7W2w8&list=WL&index=1
+Ref Zig: https://www.youtube.com/watch?v=eAM9ol7W2w8&list=WL&index=1
+
+Run help:
+zig build run in root folder
+
+Completion Indicator:
+https://operating-system-in-1000-lines.vercel.app/en/
+https://operating-system-in-1000-lines.vercel.app/en/01-setting-up-development-environment
+https://operating-system-in-1000-lines.vercel.app/en/02-assembly
+https://operating-system-in-1000-lines.vercel.app/en/03-overview
---
+++ build.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "OSZig",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addSystemCommand(&.{
+        "qemu-system-riscv32"
+    });
+    run_cmd.addArgs(&.{
+        "-machine", "virt",
+         "-bios", "default",
+         "-serial", "mon:stdio", 
+         "-nographic" ,"--no-reboot"
+    });
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_exe_unit_tests.step);
+}
