51db3192784e7b66f3dd4011a775c61a40a6d7ed
ashokmuthusamy99 <ashokmuthusamy99@gmail.com>
2025-01-19 21:54:58 +0530

    Implement kernel main function and SBI interface in Zig -- first hello world

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,22 +1,48 @@
-const bss = @extern( [*]u8,.{.name = "__bss"});
-const bss_end = @extern( [*]u8,.{.name = "__bss_end"});
-const stack_top = @extern( [*]u8,.{.name = "__stack_top"});
+const bss = @extern([*]u8, .{ .name = "__bss" });
+const bss_end = @extern([*]u8, .{ .name = "__bss_end" });
+const stack_top = @extern([*]u8, .{ .name = "__stack_top" });
 
-
-
-export fn kernel_main() noreturn{
+export fn kernel_main() noreturn {
     const bss_len = @intFromPtr(bss_end) - @intFromPtr(bss);
     @memset(bss[0..bss_len], 0);
 
-    while(true) asm volatile ("");
+    const hello = "Hello kernel!\n";
+
+    for (hello) |c| {
+        _ = sbi(c, 0, 0, 0, 0, 0, 0, 1);
+    }
+
+    while (true) asm volatile ("wfi");
 }
 
 export fn boot() linksection(".text.boot") callconv(.Naked) void {
-    _ = asm volatile(
+    _ = asm volatile (
         \\mv sp, %[stack_top]
         \\j kernel_main
-    :
-    : [stack_top] "r" (stack_top),
+        :
+        : [stack_top] "r" (stack_top),
     );
 }
 
+const SbiRet = struct { err: usize, value: usize };
+
+pub fn sbi(arg0: usize, arg1: usize, arg2: usize, arg3: usize, arg4: usize, arg5: usize, arg6: usize, arg7: usize) SbiRet {
+    var err: usize = undefined;
+    var value: usize = undefined;
+
+    asm volatile ("ecall"
+        : [err] "={a0}" (err),
+          [value] "={a1}" (value),
+        : [arg0] "{a0}" (arg0),
+          [arg1] "{a1}" (arg1),
+          [arg2] "{a2}" (arg2),
+          [arg3] "{a3}" (arg3),
+          [arg4] "{a4}" (arg4),
+          [arg5] "{a5}" (arg5),
+          [arg6] "{a6}" (arg6),
+          [arg7] "{a7}" (arg7),
+        : "memory"
+    );
+
+    return .{ .err = err, .value = value };
+}
